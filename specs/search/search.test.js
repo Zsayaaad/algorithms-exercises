d1c26ec33175c729e3407b72a791d74e55a85bff
Ziad Elsayad <136970533+Zsayaaad@users.noreply.github.com>
2024-08-06 20:59:54 +0300

    Binary Search

--- specs/search/search.test.js
+++ specs/search/search.test.js
@@ -4,55 +4,75 @@
 // it's up to you what to return if the object isn't found (we're not testing that)
 
 function linearSearch(id, array) {
-  // code goes here
+  const res = array.find((number) => number.id === id);
+
+  if (res) return res;
+  return null;
 }
 
 function binarySearch(id, array) {
-  // code goes here
+  // array must be sorted
+  let left = 0;
+  let right = array.length - 1;
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    let item = array[middle].id;
+
+    if (item < id) {
+      left = middle + 1;
+    } else if (item > id) {
+      right = middle - 1;
+    } else {
+      return array[middle];
+    }
+  }
+
+  return null;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("linear search", function () {
-  const lookingFor = { id: 5, name: "Brian" };
+test('linear search', function () {
+  const lookingFor = { id: 5, name: 'Brian' };
   expect(
     linearSearch(5, [
-      { id: 1, name: "Sam" },
-      { id: 11, name: "Sarah" },
-      { id: 21, name: "John" },
-      { id: 10, name: "Burke" },
-      { id: 13, name: "Simona" },
-      { id: 31, name: "Asim" },
-      { id: 6, name: "Niki" },
-      { id: 19, name: "Aysegul" },
-      { id: 25, name: "Kyle" },
-      { id: 18, name: "Jem" },
-      { id: 2, name: "Marc" },
-      { id: 51, name: "Chris" },
+      { id: 1, name: 'Sam' },
+      { id: 11, name: 'Sarah' },
+      { id: 21, name: 'John' },
+      { id: 10, name: 'Burke' },
+      { id: 13, name: 'Simona' },
+      { id: 31, name: 'Asim' },
+      { id: 6, name: 'Niki' },
+      { id: 19, name: 'Aysegul' },
+      { id: 25, name: 'Kyle' },
+      { id: 18, name: 'Jem' },
+      { id: 2, name: 'Marc' },
+      { id: 51, name: 'Chris' },
       lookingFor,
-      { id: 14, name: "Ben" }
+      { id: 14, name: 'Ben' },
     ])
   ).toBe(lookingFor);
 });
 
-test.skip("binary search", function () {
-  const lookingFor = { id: 23, name: "Brian" };
+test('binary search', function () {
+  const lookingFor = { id: 23, name: 'Brian' };
   expect(
     binarySearch(23, [
-      { id: 1, name: "Sam" },
-      { id: 3, name: "Sarah" },
-      { id: 5, name: "John" },
-      { id: 6, name: "Burke" },
-      { id: 10, name: "Simona" },
-      { id: 12, name: "Asim" },
-      { id: 13, name: "Niki" },
-      { id: 15, name: "Aysegul" },
-      { id: 17, name: "Kyle" },
-      { id: 18, name: "Jem" },
-      { id: 19, name: "Marc" },
-      { id: 21, name: "Chris" },
+      { id: 1, name: 'Sam' },
+      { id: 3, name: 'Sarah' },
+      { id: 5, name: 'John' },
+      { id: 6, name: 'Burke' },
+      { id: 10, name: 'Simona' },
+      { id: 12, name: 'Asim' },
+      { id: 13, name: 'Niki' },
+      { id: 15, name: 'Aysegul' },
+      { id: 17, name: 'Kyle' },
+      { id: 18, name: 'Jem' },
+      { id: 19, name: 'Marc' },
+      { id: 21, name: 'Chris' },
       lookingFor,
-      { id: 24, name: "Ben" }
+      { id: 24, name: 'Ben' },
     ])
   ).toBe(lookingFor);
 });
