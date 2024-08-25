ec4cda8b90a85b6ec35a177d8a3fafac2caaddf9
Ziad Elsayad <136970533+Zsayaaad@users.noreply.github.com>
2024-08-25 20:37:18 +0300

    Depth&Breadth Traversals

--- specs/traversals/breadth-first.test.js
+++ specs/traversals/breadth-first.test.js
@@ -1,58 +1,75 @@
+// const breadthFirstTraverse = (queue, array) => {
+//   // fill code in here
+//   if (!queue.length) return array;
+//   const currNode = queue.shift();
+//   array.push(currNode.value);
+//   if (currNode.left) queue.push(currNode.left);
+//   if (currNode.right) queue.push(currNode.right);
+//   return breadthFirstTraverse(queue, array);
+// };
+
+// recursion is not the best, doing it with looping is optimal in terms of performance
 const breadthFirstTraverse = (queue, array) => {
-  // fill code in here
+  while (queue.length) {
+    const currNode = queue.shift();
+    array.push(currNode.value);
+    if (currNode.left) queue.push(currNode.left);
+    if (currNode.right) queue.push(currNode.right);
+  }
+  return array;
 };
 
 // unit tests
 // do not modify the below code
-describe.skip("breadth-first tree traversal", function () {
-  const answer = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+describe('breadth-first tree traversal', function () {
+  const answer = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
 
   const tree = {
-    value: "A",
+    value: 'A',
     left: {
-      value: "B",
+      value: 'B',
       left: {
-        value: "D",
+        value: 'D',
         left: {
-          value: "G",
+          value: 'G',
           left: null,
-          right: null
+          right: null,
         },
-        right: null
+        right: null,
       },
       right: {
-        value: "E",
+        value: 'E',
         left: null,
         right: {
-          value: "H",
+          value: 'H',
           left: {
-            value: "K",
+            value: 'K',
             left: null,
-            right: null
-          }
-        }
-      }
+            right: null,
+          },
+        },
+      },
     },
     right: {
-      value: "C",
+      value: 'C',
       left: {
-        value: "F",
+        value: 'F',
         left: {
-          value: "I",
+          value: 'I',
           left: null,
-          right: null
+          right: null,
         },
         right: {
-          value: "J",
+          value: 'J',
           left: null,
-          right: null
-        }
+          right: null,
+        },
       },
-      right: null
-    }
+      right: null,
+    },
   };
 
-  test("breadthFirstTraverse", () => {
+  test('breadthFirstTraverse', () => {
     expect(breadthFirstTraverse([tree], [])).toEqual(answer);
   });
 });
